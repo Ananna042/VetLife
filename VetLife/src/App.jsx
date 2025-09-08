33f07f1f9cd672e52dbb221c44835fc5599056d4
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-09-08 17:07:10 -0300

    cree las router y el indice  de la pagina

---
+++ VetLife/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hola mundo</h1>
+      <p className='boton'>boton</p>
+    </>
+  )
+}
+
+export default App
